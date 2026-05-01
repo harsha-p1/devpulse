074e33681afefef9d0220ae25ef46896b1cf3fee
P Harsha Vardhan <harshavardhanpamulapati55@gmail.com>
2026-05-01 20:15:32 +0530

    Added github headers fix

--- backend/src/routes/githubRoutes.ts
+++ backend/src/routes/githubRoutes.ts
@@ -3,18 +3,28 @@ import axios from "axios";
 
 const router = express.Router();
 
+const headers = {
+  Accept: "application/vnd.github+json",
+  "User-Agent": "DevPulse-App",
+};
+
 router.get("/:username", async (req, res) => {
   try {
     const { username } = req.params;
 
     const githubUser = await axios.get(
-      `https://api.github.com/users/${username}`
+      `https://api.github.com/users/${username}`,
+      { headers }
     );
 
     res.json(githubUser.data);
 
-  } catch (error) {
-    console.log(error);
+  } catch (error: any) {
+
+    console.log(
+      "GitHub Profile Error:",
+      error.response?.data || error.message
+    );
 
     res.status(500).json({
       message: "Failed to fetch GitHub profile",
@@ -27,13 +37,18 @@ router.get("/:username/repos", async (req, res) => {
     const { username } = req.params;
 
     const githubRepos = await axios.get(
-      `https://api.github.com/users/${username}/repos`
+      `https://api.github.com/users/${username}/repos`,
+      { headers }
     );
 
     res.json(githubRepos.data);
 
-  } catch (error) {
-    console.log(error);
+  } catch (error: any) {
+
+    console.log(
+      "GitHub Repo Error:",
+      error.response?.data || error.message
+    );
 
     res.status(500).json({
       message: "Failed to fetch GitHub repos",
